2785cb430476e79d65d71576e5469c662ed3081a
damien <doute.d@gmail.com>
2025-04-11 15:35:21 -0400

    added Vines 1.15.0

--- 0.13/modify/curations-0.13.ts
+++ 0.13/modify/curations-0.13.ts
@@ -26,6 +26,13 @@ export default defineCurationLists({
           toolListUrl:
             "https://lightningrodlabs.org/weave-tool-curation/0.13/tool-list-0.13.json",
           toolId: "vines",
+          versionBranch: "1.15.x",
+          tags: ["chat"],
+        },
+        {
+          toolListUrl:
+            "https://lightningrodlabs.org/weave-tool-curation/0.13/tool-list-0.13.json",
+          toolId: "vines",
           versionBranch: "1.14.x",
           tags: ["chat"],
         },
