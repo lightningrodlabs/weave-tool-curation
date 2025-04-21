af320c26562d172743bc691ba56a54c0fc12e209
Matthias <tuttiacc@hotmail.com>
2025-04-21 19:29:53 +0200

    add 0.14 folder

---
+++ 0.14/modify/curations-0.14.ts
@@ -0,0 +1,20 @@
+import { defineCurationLists } from "@theweave/moss-types";
+
+export default defineCurationLists({
+  curator: {
+    name: "Lightningrod Labs",
+    description: "The official curation of Tools from Lightningrod Labs",
+    contact: {
+      website: "https://lightningrodlabs.org",
+    },
+    icon: "https://lightningrodlabs.org/lrl_logo.png",
+  },
+  curationLists: {
+    default: {
+      name: "Default Curation List",
+      description: "Default Curation List of Lightningrod Labs",
+      tags: [],
+      tools: [],
+    },
+  },
+});
