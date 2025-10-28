a6dd41123a6d3269d0dc7ab25f8d5d4fad34f261
ddd-mtl <doute.d@gmail.com>
2025-10-28 13:14:01 -0400

    Initial 0.15 setup

---
+++ 0.15/modify/tool-list-0.15.ts
@@ -0,0 +1,15 @@
+import { defineDevCollectiveToolList } from "@theweave/moss-types";
+
+export default defineDevCollectiveToolList({
+  developerCollective: {
+    id: "lightningrodlabs",
+    name: "Lightningrod Labs",
+    description: "Nurturing The Holochain Ecosystem",
+    icon: "https://lightningrodlabs.org/lrl_logo.png",
+    contact: {
+      website: "https://lightningrodlabs.org",
+    },
+  },
+  tools: [
+  ],
+});
